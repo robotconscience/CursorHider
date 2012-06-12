a4fab084350eb9244ffc035695bf3fcd097d7753
robotconscience <brett@robotconscience.com>
2012-06-12 18:31:30 -0400

    this should be it. be careful with this app!

---
+++ CursorHider/main.c
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  CursorHider
+//
+// Thanks, stackoverflow: http://stackoverflow.com/questions/3885896/globally-hiding-cursor-from-background-app
+//  Created by Brett Renfer on 6/12/12.
+//  Copyright (c) 2012 Rockwell Group. All rights reserved.
+//
+
+#include <ApplicationServices/ApplicationServices.h>
+
+int main(void)
+{
+    void CGSSetConnectionProperty(int, int, CFStringRef, CFBooleanRef);
+    int _CGSDefaultConnection();
+    CFStringRef propertyString;
+    
+    // Hack to make background cursor setting work
+    propertyString = CFStringCreateWithCString(NULL, "SetsCursorInBackground", kCFStringEncodingUTF8);
+    CGSSetConnectionProperty(_CGSDefaultConnection(), _CGSDefaultConnection(), propertyString, kCFBooleanTrue);
+    CFRelease(propertyString);
+    // Hide the cursor and wait
+    while(1){
+        CGDisplayHideCursor(kCGDirectMainDisplay);
+        sleep(1);
+    }
+    return 0;
+}
